ec4964c6111768edc0dafa81e4aae3c71377be1c
NomadJin <jinnykim000@gmail.com>
2019-01-16 13:41:42 +0900

    spinner and privateroute implement

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
@@ -7,6 +7,8 @@ import { clearCurrentProfile } from './actions/profileAction';
 
 import { Provider } from 'react-redux';
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -48,7 +50,9 @@ class App extends Component {
               <div className="container">
                 <Route exact path="/register" component={Register}/>
                 <Route exact path="/login" component={Login}/>
-                <Route exact path="/dashboard" component={Dashboard}/>
+                <Switch>
+                  <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+                </Switch>
               </div>
             <Footer />
           </div>
